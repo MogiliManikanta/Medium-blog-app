2cbd8a013f786e236ab8e7b4729fc1fd2e06ff1e
MogiliManikanta <you@example.com>
2024-12-29 11:21:57 +0530

    header component

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -1,9 +1,13 @@
+import { cookies } from "next/headers";
 import Header from "./header";
+import { verifyAuth } from "@/lib/auth";
 
-export default function CommonLayout({ children }) {
+export default async function CommonLayout({ children }) {
+  const token = (await cookies()).get("token")?.value;
+  const user = await verifyAuth(token);
   return (
     <div className="min-h-screen bg-white">
-      <Header />
+      {user && <Header />}
       {children}
     </div>
   );
